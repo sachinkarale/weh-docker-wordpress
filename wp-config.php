69efdd8b99419d3ebdd16f6ae50b40f3cab470c8
sachinkarale <sachin.karale@bizruntime.com>
2016-06-18 11:38:08 +0530

    database url update

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'wordpressuser');
 define('DB_PASSWORD', 'bizruntime@123');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '192.168.1.156');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
